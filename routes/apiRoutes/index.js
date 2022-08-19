0b3c547469f5311a6e3fed3b20d68dea9bb076c6
Danielle <danielle@Danielles-MacBook-Air.local>
2022-08-18 21:02:07 -0400

    Add Heroku

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,6 @@
+const fs = require("fs");
 const router = require("express").Router();
-const  notes  = require("../../db/db.json");
-const { validateNote, createNewNote } = require("../../lib/notes");
+const { notes }  = require("../../db/db.json");
 
 router.post("/notes", (req, res) => {
   // set id based on what the next index of the array will be
